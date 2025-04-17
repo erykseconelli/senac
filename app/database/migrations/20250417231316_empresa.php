cb5033965770bb5c1ff2aac9f298a045f0d88834
erykseconelli <erykseconelli2019@gmail.com>
2025-04-17 19:29:12 -0400

    commit
    Migraçao da empresa concluida

--- app/database/migrations/20250417231316_empresa.php
+++ app/database/migrations/20250417231316_empresa.php
@@ -12,11 +12,7 @@ final class Empresa extends AbstractMigration
         $table->addColumn('id', 'biginteger', ['identity' => true, 'null' => false])
             ->addColumn('empresa', 'text', ['null' => true])
             ->addColumn('cnpj', 'text', ['null' => true])
-<<<<<<< Updated upstream
             ->addColumn('endereco', 'text', ['null' => true])
-=======
-            ->addColumn('', 'text', ['null' => true])
->>>>>>> Stashed changes
             ->addColumn('ativo', 'boolean', ['null' => true, 'default' => false])
             ->addColumn('data_cadastro', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('data_atualizacao', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
